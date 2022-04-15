2e43db91f1e617c9337e9c10c48d2b58a3eb9e35
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2022-04-15 12:51:39 +0100

    Initial Commit

---
+++ gpu_inferno/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gpu_inferno
+//
+//  Created by Alexander Kuhn-Régnier on 15/04/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
