23387bf419f829605554b0d279abaf7e9d0c5f15
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2022-04-15 20:00:55 +0100

    Simplified files, only include pybind11 bindings now.

--- setup.py
+++ setup.py
@@ -6,46 +6,14 @@ from pathlib import Path
 import cmake_build_extension
 import setuptools
 
-# Importing the bindings inside the build_extension_env context manager is necessary only
-# in Windows with Python>=3.8.
-# See https://github.com/diegoferigo/cmake-build-extension/issues/8.
-# Note that if this manager is used in the init file, cmake-build-extension becomes an
-# install_requires that must be added to the setup.cfg. Otherwise, cmake-build-extension
-# could only be listed as build-system requires in pyproject.toml since it would only
-# be necessary for packaging and not during runtime.
-init_py = inspect.cleandoc(
-    """
-    import cmake_build_extension
-
-    with cmake_build_extension.build_extension_env():
-        from . import bindings
-    """
-)
-
-# This example is compliant with PEP517 and PEP518. It uses the setup.cfg file to store
-# most of the package metadata. However, build extensions are not supported and must be
-# configured in the setup.py.
 setuptools.setup(
     ext_modules=[
         cmake_build_extension.CMakeExtension(
-            name="Pybind11Bindings",
-            # Name of the resulting package name
-            install_prefix="eg_py_gpu_compute",
-            # Note: pybind11 is a build-system requirement specified in pyproject.toml,
-            #       therefore pypa/pip or pypa/build will install it in the virtual
-            #       environment created in /tmp during packaging.
-            #       This cmake_depends_on option adds the pybind11 installation path
-            #       to CMAKE_PREFIX_PATH so that the example finds the pybind11 targets
-            #       even if it is not installed in the system.
+            name="CMakePy",
+            install_prefix="py_gpu_inferno",
             cmake_depends_on=["pybind11"],
-            # Writes the content to the top-level __init__.py
-            write_top_level_init=init_py,
-            # Selects the folder where the main CMakeLists.txt is stored
-            # (it could be a subfolder)
             source_dir=str(Path(__file__).parent.absolute()),
             cmake_configure_options=[
-                # This option points CMake to the right Python interpreter, and helps
-                # the logic of FindPython3.cmake to find the active version
                 f"-DPython3_ROOT_DIR={Path(sys.prefix)}",
                 "-DCALL_FROM_SETUP_PY:BOOL=ON",
                 "-DBUILD_SHARED_LIBS:BOOL=OFF",
@@ -53,10 +21,6 @@ setuptools.setup(
         ),
     ],
     cmdclass=dict(
-        # Enable the CMakeExtension entries defined above
         build_ext=cmake_build_extension.BuildExtension,
-        # If the setup.py or setup.cfg are in a subfolder wrt the main CMakeLists.txt,
-        # you can use the following custom command to create the source distribution.
-        # sdist=cmake_build_extension.GitSdistFolder
     ),
 )
--- src/pybind.cpp
+++ src/pybind.cpp
@@ -6,8 +6,7 @@ namespace py = pybind11;
 PYBIND11_MODULE(py_gpu_inferno, m) {
     m.doc() = "GPU INFERNO";
     m.def("test", &use_metal, "A simple test function that uses metal");
-
-    pybind11::class_<GPUCompute>(m, "GPUCompute")
+    py::class_<GPUCompute>(m, "GPUCompute")
         .def(py::init<>())
         .def("run", &GPUCompute::run, "Run a metal example", py::arg("x") = 0.0f);
 }
