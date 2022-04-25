e3591a7090c4850cd1c007c0e1baa4faba92bb43
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2022-04-25 19:32:33 +0100

    First working INFERNO implementation.

--- src/inferno.hpp
+++ src/inferno.hpp
@@ -12,138 +12,271 @@
 #include <iostream>
 #include <fstream>
 #include "loadLibrary.hpp"
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <array>
+
+namespace py = pybind11;
+
+using pyArray = py::array_t<float, py::array::c_style>;
 
 class GPUCompute {
+    static const int dataSize = sizeof(float);
+    static const int paramSize = sizeof(int);
+
+    static const int landPts = 7771;
+    static const int nPFTGroups = 3;
+    static const int nPFT = 13;
+    static const int nTotalPFT = 17;
+
     NS::AutoreleasePool* autoreleasePool;
     MTL::Device* device;
     MTL::Library* library;
     MTL::Function* fn;
-    MTL::ComputePipelineState* computePSO;
+    MTL::ComputePipelineState* computePipelineState;
 
-    static const int N = 5;
-    static const int dataSize = sizeof(float);
+    MTL::Buffer* outputBuffer;
 
-    MTL::Buffer* pBuffer;
-    MTL::CommandQueue* pCommandQueue;
-    MTL::CommandBuffer* pCommandBuffer;
+    static const int nData = 17;
+    static const int nParam = 18;
+    std::array<MTL::Buffer*, nData> dataBuffers;
+    std::array<MTL::Buffer*, nParam> paramBuffers;
 
-    MTL::ComputeCommandEncoder* pComputeEncoder;
+    MTL::ArgumentEncoder* argumentEncoder;
+    MTL::Buffer* argumentBuffer;
 
-    NS::UInteger threadGroupSize;
+    MTL::CommandQueue* commandQueue;
+    MTL::CommandBuffer* commandBuffer;
+
+    MTL::ComputeCommandEncoder* computeCommandEncoder;
+
+    MTL::Size threadgroupSize;
+
+    // Parameters.
+    int ignitionMethod;
+    int flammabilityMethod;
+    int drynessMethod;
+    int fuelBuildUpMethod;
+    int includeTemperature;
+    int Nt;
+    long outputCount;
+
+    // Misc.
+    bool didSetData = false;
+    bool didSetParams = false;
+    bool didRelease = false;
+
+    MTL::Buffer* createBufferFromPyArray(pyArray array) {
+        py::buffer_info buf = array.request();
+        return device->newBuffer(static_cast<float *>(buf.ptr), buf.shape[0] * dataSize, MTL::ResourceOptions());
+    }
 
 public:
     GPUCompute() {
         std::cout << "Init" << std::endl;
         autoreleasePool = NS::AutoreleasePool::alloc()->init();
         device = MTL::CreateSystemDefaultDevice();
+        commandQueue = device->newCommandQueue();
 
         NS::Error* error = nullptr;
 
         library = loadLibrary(device);
 
-        fn = library->newFunction( NS::String::string("test_fn", NS::UTF8StringEncoding) );
+        fn = library->newFunction( NS::String::string("multi_timestep_inferno", NS::UTF8StringEncoding) );
 
-        computePSO = device->newComputePipelineState( fn, &error );
-        if ( !computePSO )
+        argumentEncoder = fn->newArgumentEncoder(7);
+        argumentBuffer = device->newBuffer(argumentEncoder->encodedLength(), MTL::ResourceOptions());
+        argumentEncoder->setArgumentBuffer(argumentBuffer, 0);
+
+        computePipelineState = device->newComputePipelineState( fn, &error );
+        if ( !computePipelineState )
         {
             __builtin_printf( "%s", error->localizedDescription()->utf8String() );
             assert(false);
         }
-        threadGroupSize = computePSO->maxTotalThreadsPerThreadgroup();
 
-        pBuffer = device->newBuffer(dataSize * N, MTL::ResourceStorageModeManaged);
+        threadgroupSize = MTL::Size(computePipelineState->maxTotalThreadsPerThreadgroup(), 1, 1);
+    }
+
+    void release() {
+        std::cout << "Releasing pool" << std::endl;
+        autoreleasePool->release();
+        didRelease = true;
     }
 
     ~GPUCompute() {
         std::cout << "Destructor called" << std::endl;
-        autoreleasePool->release();
-    }
-
-    void run(float x = 0.0f) {
-        pCommandQueue = device->newCommandQueue();
-        pCommandBuffer = pCommandQueue->commandBuffer();
-        assert(pCommandBuffer);
-        pComputeEncoder = pCommandBuffer->computeCommandEncoder();
-
-        pComputeEncoder->setComputePipelineState( computePSO );
-
-        pComputeEncoder->setBuffer( pBuffer, 0, 0 );
-
-        pComputeEncoder->setBytes( &x, sizeof(float), 1);
-
-        MTL::Size gridSize = MTL::Size( N, 1, 1 );
-
-        MTL::Size threadgroupSize( threadGroupSize, 1, 1 );
-
-        pComputeEncoder->dispatchThreads( gridSize, threadgroupSize );
-        pComputeEncoder->endEncoding();
-        pCommandBuffer->commit();
-        pCommandBuffer->waitUntilCompleted();
-
-        float* bufferContents = (float*)pBuffer->contents();
-
-        std::cout << "Contents:" << "\n";
-        for (int i = 0 ; i < N ; ++i) {
-            std::cout << *bufferContents << "\n";
-            bufferContents++;
+        if (!(didRelease)) {
+            autoreleasePool->release();
         }
     }
+
+    void set_data(
+        int _ignitionMethod,
+        int _flammabilityMethod,
+        int _drynessMethod,
+        int _fuelBuildUpMethod,
+        int _includeTemperature,
+        int _Nt,  // i.e. <data>.shape[0].
+        // -------------- Start data arrays --------------
+        // Input arrays.
+        pyArray t1p5m_tile,
+        pyArray q1p5m_tile,
+        pyArray pstar,
+        // XXX NOTE - This is with a single soil layer selected!
+        pyArray sthu_soilt_single,
+        pyArray frac,
+        pyArray c_soil_dpm_gb,
+        pyArray c_soil_rpm_gb,
+        pyArray canht,
+        pyArray ls_rain,
+        pyArray con_rain,
+        pyArray pop_den,
+        pyArray flash_rate,
+        pyArray fuel_build_up,
+        pyArray fapar_diag_pft,
+        pyArray grouped_dry_bal,
+        pyArray litter_pool,
+        pyArray dry_days
+        // -------------- End data arrays --------------
+    ) {
+        // Set parameters.
+        ignitionMethod = _ignitionMethod;
+        flammabilityMethod = _flammabilityMethod;
+        drynessMethod = _drynessMethod;
+        fuelBuildUpMethod = _fuelBuildUpMethod;
+        includeTemperature = _includeTemperature;
+        Nt = _Nt;
+
+        // Create buffers.
+        std::array<pyArray, nData> dataArrays = {
+            t1p5m_tile,
+            q1p5m_tile,
+            pstar,
+            sthu_soilt_single,
+            frac,
+            c_soil_dpm_gb,
+            c_soil_rpm_gb,
+            canht,
+            ls_rain,
+            con_rain,
+            pop_den,
+            flash_rate,
+            fuel_build_up,
+            fapar_diag_pft,
+            grouped_dry_bal,
+            litter_pool,
+            dry_days,
+        };
+
+        for (unsigned long i = 0; i < nData; i++) {
+            dataBuffers[i] = createBufferFromPyArray(dataArrays[i]);
+            argumentEncoder->setBuffer(dataBuffers[i], 0, i);
+        }
+
+        outputCount = Nt * nPFT * landPts;
+        outputBuffer = device->newBuffer(outputCount * dataSize, MTL::ResourceOptions());
+
+        didSetData = true;
+    }
+
+    void set_params(
+        pyArray fapar_factor,
+        pyArray fapar_centre,
+        pyArray fapar_shape,
+        pyArray fuel_build_up_factor,
+        pyArray fuel_build_up_centre,
+        pyArray fuel_build_up_shape,
+        pyArray temperature_factor,
+        pyArray temperature_centre,
+        pyArray temperature_shape,
+        pyArray dry_day_factor,
+        pyArray dry_day_centre,
+        pyArray dry_day_shape,
+        pyArray dry_bal_factor,
+        pyArray dry_bal_centre,
+        pyArray dry_bal_shape,
+        pyArray litter_pool_factor,
+        pyArray litter_pool_centre,
+        pyArray litter_pool_shape
+    ) {
+        // Create buffers.
+        std::array<pyArray, nParam> paramArrays = {
+            fapar_factor,
+            fapar_centre,
+            fapar_shape,
+            fuel_build_up_factor,
+            fuel_build_up_centre,
+            fuel_build_up_shape,
+            temperature_factor,
+            temperature_centre,
+            temperature_shape,
+            dry_day_factor,
+            dry_day_centre,
+            dry_day_shape,
+            dry_bal_factor,
+            dry_bal_centre,
+            dry_bal_shape,
+            litter_pool_factor,
+            litter_pool_centre,
+            litter_pool_shape,
+        };
+
+        for (unsigned long i = 0; i < nParam; i++) {
+            paramBuffers[i] = createBufferFromPyArray(paramArrays[i]);
+        }
+        didSetParams = true;
+    }
+
+    pyArray run() {
+        if (!(didSetParams && didSetData)) {
+            if (!(didSetData)) __builtin_printf("Did not set data.");
+            if (!(didSetParams)) __builtin_printf("Did not set params.");
+            assert(false);
+        }
+
+        commandBuffer = commandQueue->commandBuffer();
+        assert(commandBuffer);
+
+        computeCommandEncoder = commandBuffer->computeCommandEncoder();
+
+        computeCommandEncoder->setComputePipelineState(computePipelineState);
+
+        // Output.
+        computeCommandEncoder->setBuffer(outputBuffer, 0, 0);
+
+        // Parameters.
+        computeCommandEncoder->setBytes(&ignitionMethod, paramSize, 1);
+        computeCommandEncoder->setBytes(&flammabilityMethod, paramSize, 2);
+        computeCommandEncoder->setBytes(&drynessMethod, paramSize, 3);
+        computeCommandEncoder->setBytes(&fuelBuildUpMethod, paramSize, 4);
+        computeCommandEncoder->setBytes(&includeTemperature, paramSize, 5);
+        computeCommandEncoder->setBytes(&Nt, paramSize, 6);
+
+        // Data arrays.
+        computeCommandEncoder->setBuffer(argumentBuffer, 0, 7);
+        for (unsigned long i = 0; i < nData; i++) {
+            computeCommandEncoder->useResource(dataBuffers[i], MTL::ResourceUsageRead);
+        }
+
+        // Parameter arrays.
+        for (unsigned long i = 0; i < nParam; i++) {
+            computeCommandEncoder->setBuffer(paramBuffers[i], 0, 8 + i);
+        }
+
+        MTL::Size gridSize = MTL::Size(landPts * nPFT, 1, 1);
+
+        computeCommandEncoder->dispatchThreads(gridSize, threadgroupSize);
+        computeCommandEncoder->endEncoding();
+        commandBuffer->commit();
+
+        commandBuffer->waitUntilCompleted();
+
+        return pyArray(
+            outputCount,
+            (float*)outputBuffer->contents()
+        );
+    }
 };
-
-void use_metal() {
-    NS::AutoreleasePool* pAutoreleasePool = NS::AutoreleasePool::alloc()->init();
-
-    std::cout << "Starting!\n";
-
-    MTL::Device* _pDevice = MTL::CreateSystemDefaultDevice();
-
-    NS::Error* pError = nullptr;
-
-    MTL::Library* pLibrary = loadLibrary(_pDevice);
-
-    MTL::Function* testFn = pLibrary->newFunction( NS::String::string("test_fn", NS::UTF8StringEncoding) );
-    MTL::ComputePipelineState* _pComputePSO = _pDevice->newComputePipelineState( testFn, &pError );
-    if ( !_pComputePSO )
-    {
-        __builtin_printf( "%s", pError->localizedDescription()->utf8String() );
-        assert(false);
-    }
-
-    int N = 5;
-    int dataSize = sizeof(float);
-
-    MTL::Buffer* _pBuffer = _pDevice->newBuffer(dataSize * N, MTL::ResourceStorageModeManaged);
-
-    MTL::CommandQueue* _pCommandQueue = _pDevice->newCommandQueue();
-
-    MTL::CommandBuffer* pCommandBuffer = _pCommandQueue->commandBuffer();
-    assert(pCommandBuffer);
-
-    MTL::ComputeCommandEncoder* pComputeEncoder = pCommandBuffer->computeCommandEncoder();
-
-    pComputeEncoder->setComputePipelineState( _pComputePSO );
-
-    pComputeEncoder->setBuffer( _pBuffer, 0, 0 );
-
-    MTL::Size gridSize = MTL::Size( N, 1, 1 );
-
-    NS::UInteger threadGroupSize = _pComputePSO->maxTotalThreadsPerThreadgroup();
-    MTL::Size threadgroupSize( threadGroupSize, 1, 1 );
-
-    pComputeEncoder->dispatchThreads( gridSize, threadgroupSize );
-    pComputeEncoder->endEncoding();
-    pCommandBuffer->commit();
-    pCommandBuffer->waitUntilCompleted();
-
-    float* bufferContents = (float*)_pBuffer->contents();
-
-    std::cout << "Contents:" << "\n";
-    for (int i = 0 ; i < N ; ++i) {
-        std::cout << *bufferContents << "\n";
-        bufferContents++;
-    }
-
-    pAutoreleasePool->release();
-}
 
 #endif
--- src/pybind.cpp
+++ src/pybind.cpp
@@ -1,12 +1,59 @@
 #include "inferno.hpp"
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
 namespace py = pybind11;
 
+
 PYBIND11_MODULE(py_gpu_inferno, m) {
     m.doc() = "GPU INFERNO";
-    m.def("test", &use_metal, "A simple test function that uses metal");
     py::class_<GPUCompute>(m, "GPUCompute")
         .def(py::init<>())
-        .def("run", &GPUCompute::run, "Run a metal example", py::arg("x") = 0.0f);
+        .def("set_data", &GPUCompute::set_data, "Set input data.",
+            py::arg("_ignitionMethod"),
+            py::arg("_flammabilityMethod"),
+            py::arg("_drynessMethod"),
+            py::arg("_fuelBuildUpMethod"),
+            py::arg("_includeTemperature"),
+            py::arg("_Nt"),
+            py::arg("t1p5m_tile"),
+            py::arg("q1p5m_tile"),
+            py::arg("pstar"),
+            py::arg("sthu_soilt_single"),
+            py::arg("frac"),
+            py::arg("c_soil_dpm_gb"),
+            py::arg("c_soil_rpm_gb"),
+            py::arg("canht"),
+            py::arg("ls_rain"),
+            py::arg("con_rain"),
+            py::arg("pop_den"),
+            py::arg("flash_rate"),
+            py::arg("fuel_build_up"),
+            py::arg("fapar_diag_pft"),
+            py::arg("grouped_dry_bal"),
+            py::arg("litter_pool"),
+            py::arg("dry_days")
+        )
+        .def("set_params", &GPUCompute::set_params, "Set input parameters.",
+            py::arg("fapar_factor"),
+            py::arg("fapar_centre"),
+            py::arg("fapar_shape"),
+            py::arg("fuel_build_up_factor"),
+            py::arg("fuel_build_up_centre"),
+            py::arg("fuel_build_up_shape"),
+            py::arg("temperature_factor"),
+            py::arg("temperature_centre"),
+            py::arg("temperature_shape"),
+            py::arg("dry_day_factor"),
+            py::arg("dry_day_centre"),
+            py::arg("dry_day_shape"),
+            py::arg("dry_bal_factor"),
+            py::arg("dry_bal_centre"),
+            py::arg("dry_bal_shape"),
+            py::arg("litter_pool_factor"),
+            py::arg("litter_pool_centre"),
+            py::arg("litter_pool_shape")
+        )
+        .def("run", &GPUCompute::run, "Run kernel with defined data and parameters.")
+        .def("release", &GPUCompute::release, "Release autorelease pool.");
 }
